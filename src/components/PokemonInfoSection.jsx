7c1b8a9fae0d2a2ad26b8709b8fe2521ff7fb6f4
divyanshdahiya <dahiya.divyansh1@gmail.com>
2021-10-21 21:00:21 +0530

    added all the 2nd fetched info

--- src/components/PokemonInfoSection.jsx
+++ src/components/PokemonInfoSection.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "../index.css";
 import axios from "axios";
 
 function PokemonInfoSection({ pokemon }) {
@@ -9,14 +10,25 @@ function PokemonInfoSection({ pokemon }) {
       const res = await axios.get(
         `https://pokeapi.co/api/v2/pokemon/${pokemon}`
       );
-      console.log(res.data);
+      console.log(res.data.stats[5]);
+      // console.log(res.data.abilities[0].ability.name);
+      // console.log(res.data.abilities[1].ability.name);
       setpokemonInfo({
+        image: res.data.sprites.front_default,
         experience: res.data.base_experience,
         weight: res.data.weight,
         height: res.data.height,
-        abilities: res.data.abilities,
-        moves: res.data.moves,
-        stats: res.data.stats,
+        ability1: res.data.abilities[0].ability.name,
+        ability2: res.data.abilities[1].ability.name,
+        move1: res.data.moves[0].move.name,
+        move2: res.data.moves[1].move.name,
+        move3: res.data.moves[2].move.name,
+        hp: res.data.stats[0].base_stat,
+        attack: res.data.stats[1].base_stat,
+        defense: res.data.stats[2].base_stat,
+        special_attack: res.data.stats[3].base_stat,
+        special_defence: res.data.stats[4].base_stat,
+        speed: res.data.stats[5].base_stat,
       });
     }
     getData();
@@ -24,9 +36,35 @@ function PokemonInfoSection({ pokemon }) {
 
   return (
     <div>
-      <h1>{pokemonInfo.experience}</h1>
-      <h1>{pokemonInfo.weight}</h1>
-      <h1>{pokemonInfo.height}</h1>
+      <h1 className="poke-heading">{pokemon}</h1>
+      <img src={pokemonInfo.image} alt="pokemon-logo" className="poke-image" />
+
+      <div className="poke-info-container">
+        <div className="poke-info">
+          <h2>Pokemon Experience -</h2>
+          <h2> {pokemonInfo.experience}</h2>
+        </div>
+        <div className="poke-info">
+          <h2>Pokemon weight -</h2>
+          <h2>{pokemonInfo.weight} kg</h2>
+        </div>
+        <div className="poke-info">
+          <h2>Pokemon Height -</h2>
+          <h2>{pokemonInfo.height} m</h2>
+        </div>
+      </div>
+
+      <h2>{pokemonInfo.ability1}</h2>
+      <h2>{pokemonInfo.ability2}</h2>
+      <h2>{pokemonInfo.move1}</h2>
+      <h2>{pokemonInfo.move2}</h2>
+      <h2>{pokemonInfo.move3}</h2>
+      <h2>{pokemonInfo.hp}</h2>
+      <h2>{pokemonInfo.attack}</h2>
+      <h2>{pokemonInfo.defence}</h2>
+      <h2>{pokemonInfo.special_attack}</h2>
+      <h2>{pokemonInfo.special_defence}</h2>
+      <h2>{pokemonInfo.speed}</h2>
     </div>
   );
 }
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -5,7 +5,7 @@ function PokemonList({ setPokemon, pokemonList }) {
   return (
     <div className="pokemon-list">
       {pokemonList.map((p) => (
-        <div key={p}>
+        <div className="pokemon-list-items" key={p}>
           <li href="" onClick={() => setPokemon(p)}>
             {p}
           </li>
